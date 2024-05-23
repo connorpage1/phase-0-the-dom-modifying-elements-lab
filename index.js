162b220c10b74383dd9151810f408ddb3c7eb9da
Connor Page <george@connorpage.com>
2024-05-22 22:32:39 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementById('main');
+  main.remove();
+
+  const newHeader = document.createElement('h1');
+
+  newHeader.setAttribute('id', 'victory');
+
+  const yourName = "Connor"; // Replace "Your Name" with your actual name
+  newHeader.textContent = `${yourName} is the champion`;
+
+  document.body.appendChild(newHeader);
